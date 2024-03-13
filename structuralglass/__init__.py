612cae54430bd9c3d41978f8ff90075fbefc77aa
Norman J.N. Richardson <normanrichardson@gmail.com>
2024-03-13 14:21:27 -0700

    Update resource reading for changes in pint

--- structuralglass/__init__.py
+++ structuralglass/__init__.py
@@ -1,17 +1,12 @@
 import pint
+import importlib.resources as pkg_resources
 
 from . import resources
 
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    # Try backported to PY<37 `importlib_resources`.
-    import importlib_resources as pkg_resources
-
-# Load the file stream for the units file
-unit_file = pkg_resources.open_text(resources, "unit_def.txt")
+# look up the resource for the units file
+unit_file=pkg_resources.files(resources).joinpath("unit_def.txt")
 
 # Setup pint for the package
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
-ureg.load_definitions(unit_file)
+ureg.load_definitions(str(unit_file))
